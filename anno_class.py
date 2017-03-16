77e5f15299a3ecceab688ebec38df4e26b211e4f
晓燕王 <xiaoyanwang@MacBook-Pro-2.local>
2017-03-16 23:16:08 +0800

    anno_class.py

--- anno_class.py
+++ anno_class.py
@@ -1,6 +1,5 @@
 import re
 import cv2
-import gen_xml as gen
 class PASemptyrecord:
 	def __init__(self):
 		self.imgname = ''
@@ -27,9 +26,53 @@ class AnnotationInfo:
 		self.pattern3 = re.compile(r'Database : "(.+)"')
 		self.pattern4 = re.compile(r'Bounding box for object \d+ ".+" \(Xmin, Ymin\) - \(Xmax, Ymax\) : \((\d+), (\d+)\) - \((\d+), (\d+)\)')
 		self.pattern5 = re.compile(r'Original label for object \d+ ".+" : "(.+)"')
-                self.caltech1 = re.compile(r'num: (\d+)')
+                
+		self.caltech1 = re.compile(r'num: (\d+)')
                 self.caltech2 = re.compile(r'label: (.+)')
-                self.caltech3 = re.compile(r'pos: (\d+
+                self.caltech3 = re.compile(r'pos: (\d+\.\d+) (\d+\.\d+) (\d+\.\d+) (\d+\.\d+)')
+		self.caltech4 = re.compile(r'occl: (\d+)')
+		self.caltech5 = re.compile(r'lock: (\d+)')
+		self.caltech6 = re.compile(r'posv: (\d+\.\d+) (\d+\.\d+) (\d+\.\d+) (\d+\.\d+)')
+
+        def match_caltech_mode(self, line):
+		match = self.caltech2.match(line)
+		if match:
+			tmp = match.group(1)
+			self.record.originlabel.append(tmp)
+			return
+
+		match = self.caltech3.match(line)
+		if match:
+			posx1 = float(match.group(1))
+			posy1 = float(match.group(2))
+			posx2 = float(match.group(3)) + posx1
+			posy2 = float(match.group(4)) + posy1
+			self.record.xmin.append(str(int(round(posx1))))
+			self.record.ymin.append(str(int(round(posy1))))
+			self.record.xmax.append(str(int(round(posx2))))
+			self.record.ymax.append(str(int(round(posy2))))
+			return
+		match = self.caltech4.match(line)
+		if match:
+			self.record.occl.append(match.group(1))
+			return
+		match = self.caltech5.match(line)
+		if match:
+			self.record.lock.append(match.group(1))
+			return
+		match = self.caltech6.match(line)
+		if match:
+			posvx1 = float(match.group(1))
+			posvy1 = float(match.group(2))
+			posvx2 = float(match.group(3)) + posvx1
+			posvy2 = float(match.group(4)) + posvy1
+			self.record.vxmin.append(str(int(round(posvx1))))
+			self.record.vymin.append(str(int(round(posvy1))))
+			self.record.vxmax.append(str(int(round(posvx2))))
+			self.record.vymax.append(str(int(round(posvy2))))
+			return
+			
+
 	def match_mode(self,line):
 		match = self.pattern1.match(line)
 		if match:
@@ -62,17 +105,7 @@ class AnnotationInfo:
 			return 
 #		print 'ERROR, NO MACHES FOUND!'
 	
-        def match_caltech_mode(self, line):
-                
-	def handle_pascal_txt(self, pastxt):
-		f = open(pastxt, 'r')
-		lines = f.readlines()
-		f.close()
-		for line in lines:
-			self.match_mode(line)
-        def handle_kitti_txt(self, kitti_txt):
-                pass
-        def handle_caltech_txt(self, caltech_txt), jpeg):
+	def handle_caltech_txt(self, caltech_txt), jpeg):
                 img = cv2.imread(jpeg)
                 self.record.height = str(img.shape[0])
                 self.record.weight = str(img.shape[1])
@@ -82,7 +115,18 @@ class AnnotationInfo:
                 f = open(caltech_txt, 'r')
                 lines = f.readlines()
                 f.close()
+		for line in lines:
+			self.match_caltech_mode(line)
 
+        
+	def handle_pascal_txt(self, pastxt):
+		f = open(pastxt, 'r')
+		lines = f.readlines()
+		f.close()
+		for line in lines:
+			self.match_mode(line)
+        def handle_kitti_txt(self, kitti_txt):
+                pass
             
         def handle_jpeg(self, jpeg):
                 img = cv2.imread(jpeg)
